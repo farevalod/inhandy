72e4135bead00a8446a4c63c11f6521268b6ae37
Francisco Arevalo <farevalod@gmail.com>
2013-03-12 17:59:47 -0300

    Metodo producto average score

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
   attr_accessible :area, :fingerptint, :name, :variety, :year
   belongs_to :vineyard
+  has_many :posts
+
+  def average_score
+    @suma = 0
+	self.posts.each do |p|
+		@suma += p.score
+	end
+ 	return (@suma/self.posts.count.to_f).round(2)
+  end
 end
