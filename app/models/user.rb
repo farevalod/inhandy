254ceba84d60fa013e13ae7afdc0a0c80af20c04
Paul Nicolet <paulyarur@gmail.com>
2013-03-12 23:51:46 -0300

    Relationships commit 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,24 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :name, :password_confirmation, :remember_me, :bio
   # attr_accessible :title, :body
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 end
